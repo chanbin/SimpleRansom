412d8250d2b45f7f716f4ffc6f469f8cb2e48a91
chanbin <chanbining@gmail.com>
2024-08-27 08:35:16 +0900

    Update ransom_recovery.py

--- ransom_recovery.py
+++ ransom_recovery.py
@@ -7,5 +7,5 @@ for root, dirs, files in os.walk((".").encode('utf8')):#..\\pentest2
 		if file.endswith((".메롱").encode('utf8')):
 			target = '"' + os.path.join(root, file).decode('utf8') +'"'
 			print(target)
-			os.system(base_path+'\\openssl\\bin\\conf.exe enc -aes-256-ofb -d -in '+target+' -pass pass:rufqls12; > '+target[:-3]+'"')
+			os.system(base_path+'\\openssl\\bin\\conf.exe enc -aes-256-ofb -d -in '+target+' -pass pass:{pass} > '+target[:-3]+'"')
 			os.system('del /F ' + target + ' 1> nul 2>&1')
